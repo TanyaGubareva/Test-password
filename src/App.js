2df714fab0e0f0fdd018e5cdf5f37e3d887cefd0
TanyaGubareva <tanyagubareva007@gmail.com>
2023-02-02 09:14:10 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,117 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import "./App";
 function App() {
+  const [value, setValue] = useState("");
+
+  const onInput = (e) => {
+    setValue(e.currentTarget.value);
+  };
+
+  const valueOnlySymbols = value.replaceAll(/\d+|[a-zA-Zа-яА-Я]+/gm, "");
+  const isExistsSymbols = valueOnlySymbols.length !== 0;
+  const isOnlySymbols = valueOnlySymbols.length === value.length;
+  const isSmallLetters = value.length < 8;
+  const isEmptyInput = value.length === 0;
+
+  const isEasy =
+    /^\d+$/gm.test(value) || /^[a-zA-Zа-яА-Я]+$/gm.test(value) || isOnlySymbols;
+
+  const isExistsNumber = /\d/gm.test(value);
+  const isExistsLetters = /[a-zA-Zа-яА-Я]/gm.test(value);
+
+  const isMedium =
+    (isExistsNumber && isExistsSymbols) ||
+    (isExistsLetters && isExistsNumber) ||
+    (isExistsLetters && isExistsSymbols);
+
+  const isHard = isExistsLetters && isExistsNumber && isExistsSymbols;
+
+  const stepOne = () => {
+    if (isEmptyInput) {
+      return {
+        backgroundColor: "grey",
+      };
+    }
+
+    if (isSmallLetters) {
+      return { backgroundColor: "red" };
+    }
+
+    if (isEasy) {
+      return { backgroundColor: "red" };
+    }
+    if (isHard) {
+      return { backgroundColor: "green" };
+    }
+    if (isMedium) {
+      return { backgroundColor: "yellow" };
+    }
+  };
+
+  const stepTwo = () => {
+    if (isEmptyInput) {
+      return { backgroundColor: "grey" };
+    }
+
+    if (isSmallLetters) {
+      return { backgroundColor: "red" };
+    }
+
+    if (isEasy) {
+      return { backgroundColor: "grey" };
+    }
+
+    if (isHard) {
+      return { backgroundColor: "green" };
+    }
+
+    if (isMedium) {
+      return { backgroundColor: "yellow" };
+    }
+  };
+
+  const stepThree = () => {
+    if (isEmptyInput) {
+      return {
+        backgroundColor: "grey",
+      };
+    }
+
+    if (isSmallLetters) {
+      return { backgroundColor: "red" };
+    }
+
+    if (isHard) {
+      return { backgroundColor: "green" };
+    }
+
+    if (isEasy || isMedium) {
+      return { backgroundColor: "grey" };
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        backgroundColor: "#d5fdff",
+        borderRadius: "10px",
+        padding: "20px",
+        width: "800px",
+      }}
+    >
+      <p style={{ fontWeight: "800" }}>Password</p>
+      <input
+        value={value}
+        onInput={onInput}
+        className="password"
+        placeholder="Enter your password.."
+      />
+      <p style={stepOne()}>Only letters/digits/symbols</p>
+      <p style={stepTwo()}>
+        Combination of letters-symbols/letters-digits/digits-symbols
+      </p>
+      <p style={stepThree()}>
+        Combination of letters-symbols/letters-digits/digits-symbols
+      </p>
     </div>
   );
 }
